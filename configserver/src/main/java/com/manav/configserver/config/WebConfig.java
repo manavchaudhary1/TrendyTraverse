44d41060571e0ae5921f1688d9a27a95568426dd
manavchaudhary1 <manavchaudhary041@gmail.com>
2025-02-22 00:36:03 +0530

    Added config-server

---
+++ configserver/src/main/java/com/manav/configserver/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.manav.configserver.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.filter.RequestContextFilter;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    @ConditionalOnMissingBean(RequestContextListener.class)
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
+    }
+
+    @Bean
+    public RequestContextFilter requestContextFilter() {
+        return new RequestContextFilter();
+    }
+}
