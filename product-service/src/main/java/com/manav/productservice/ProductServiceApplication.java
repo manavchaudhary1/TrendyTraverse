97a9da22e966578010f9c4fa968216b2c7b037a8
manavchaudhary1 <manavchaudhary041@gmail.com>
2025-02-22 02:43:53 +0530

    Configured ProductService for ConfigServer

--- product-service/src/main/java/com/manav/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/manav/productservice/ProductServiceApplication.java
@@ -2,8 +2,10 @@ package com.manav.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
