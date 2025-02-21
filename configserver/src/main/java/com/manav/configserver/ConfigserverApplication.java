c6f4802901473c03b09a0132e756d25a978831b3
manavchaudhary1 <manavchaudhary041@gmail.com>
2025-02-22 00:45:25 +0530

    Added .env file support

--- configserver/src/main/java/com/manav/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/manav/configserver/ConfigserverApplication.java
@@ -1,16 +1,22 @@
 package com.manav.configserver;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+import static io.github.cdimascio.dotenv.Dotenv.load;
 
 @SpringBootApplication
 @EnableConfigServer
 @RefreshScope
 public class ConfigserverApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        Dotenv dotenv = load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(ConfigserverApplication.class, args);
     }
 
