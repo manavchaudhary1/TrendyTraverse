ec4c2c5daf95fab54606553a009471606f768251
manavchaudhary1 <manavchaudhary041@gmail.com>
2025-03-04 00:12:04 +0530

    Added SecurityConfig for Authorization and JwtAuthConverter for token read

---
+++ cart-service/src/main/java/com/manav/cartservice/config/JwtAuthConverter.java
@@ -0,0 +1,67 @@
+package com.manav.cartservice.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+@Slf4j
+public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+    @Value("${jwt.auth.converter.principle-attribute}")
+    private String principleAttribute;
+
+    @Value("${jwt.auth.converter.resource-id}")
+    private String resourceId;
+
+    @Override
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream.concat(
+                Optional.ofNullable(jwtGrantedAuthoritiesConverter.convert(jwt)).orElse(Collections.emptyList()).stream(),
+                extractResourceRoles(jwt).stream()
+        ).collect(Collectors.toSet());
+
+        return new JwtAuthenticationToken(jwt, authorities, getPrincipalClaimName(jwt));
+    }
+
+    private String getPrincipalClaimName(Jwt jwt) {
+        return jwt.getClaim(principleAttribute) != null ? jwt.getClaim(principleAttribute) : jwt.getSubject();
+    }
+
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
+        if (resourceAccess == null || !resourceAccess.containsKey(resourceId)) {
+            return Set.of();
+        }
+
+        Object resourceObj = resourceAccess.get(resourceId);
+        if (!(resourceObj instanceof Map)) {
+            return Set.of();
+        }
+
+        Map<String, Object> resource = (Map<String, Object>) resourceObj;
+
+        Object rolesObj = resource.get("roles");
+        if (!(rolesObj instanceof Collection)) {
+            return Set.of();
+        }
+
+        Collection<String> resourceRoles = (Collection<String>) rolesObj;
+        return resourceRoles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+}
